46e0c306f16af279f034758bd098fe3b1153e64e
GiuranRadu <117910448+GiuranRadu@users.noreply.github.com>
2024-05-08 10:45:14 +0300

    add start all servers

--- src/Components/AppLayout/AppLayout.jsx
+++ src/Components/AppLayout/AppLayout.jsx
@@ -7,8 +7,15 @@ function AppLayout() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch('https://formula1-backend.onrender.com/');
-        console.log(res);
+        const res1 = await fetch('https://formula1-backend.onrender.com/');
+        const res2 = await fetch('https://ur-physique-backend.onrender.com/');
+        const res3 = await fetch('https://x-games-backend.onrender.com/');
+        const res4 = await fetch('https://z-animals-backend.onrender.com/');
+        console.log(`F1 status ${res1.status}`);
+        console.log(`Ur-physique status ${res2.status}`);
+        console.log(`X-Games status ${res3.status}`);
+        console.log(`Z-Animals status ${res4.status}`);
+
       } catch (error) {
         console.error('Error fetching data:', error);
       }
